a2c81e297ecb30961b71d4b9144dcf19b9de57e5
JorgePatricio <65610111+JorgePatricio@users.noreply.github.com>
2021-07-20 09:57:33 -0300

    CRUD Scaf

---
+++ SalesWebTest/Data/SalesWebTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebTest.Models;
+
+namespace SalesWebTest.Data
+{
+    public class SalesWebTestContext : DbContext
+    {
+        public SalesWebTestContext (DbContextOptions<SalesWebTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebTest.Models.Department> Department { get; set; }
+    }
+}
